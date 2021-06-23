67e7a4bc518338cec639b4b89aeb79c30578fec8
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-23 10:26:45 -0500

    added dropdowns for Cardiac Arrest form - editing dropdown names now

--- src/components/PatientCardiac/PatientCardiac.jsx
+++ src/components/PatientCardiac/PatientCardiac.jsx
@@ -1,11 +1,221 @@
 import React from 'react';
 
+// ----- Material UI -----
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+
 function PatientCardiac() {
 
     return (
         <div className="container">
             <h2>Patient Cardiac Arrest Form:</h2>
-            <button>911 Heart Probz</button>
+            <button>911 Heart Probz</button> <br /><br />
+
+            <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
         </div>
     )
 }
