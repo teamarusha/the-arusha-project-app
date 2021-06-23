9273bf8e886339f84e0a12104131b6702343d5db
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-23 10:00:45 -0500

    added inputs & dropdowns for Symptoms form

--- src/components/PatientSymptoms/PatientSymptoms.jsx
+++ src/components/PatientSymptoms/PatientSymptoms.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// ----- Material UI -----
+import { TextField } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+
 function PatientSymptoms() {
 
     return (
@@ -23,6 +29,66 @@ function PatientSymptoms() {
                     <MenuItem value={20}>Option 2</MenuItem>
                     <MenuItem value={30}>Option 3</MenuItem>
                 </Select> <br /><br />
+
+                <InputLabel 
+                id="demo-simple-select-autowidth-label">Chief Complaint Organ System</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <p>Symptom Onset calendar goes here</p> <br />
+
+                <TextField id="outlined-basic" label="Primary Symptom" variant="outlined">
+                </TextField> &nbsp;<br /><br />
+                <TextField id="outlined-basic" label="Other Associated Symptoms" variant="outlined">
+                </TextField> <br /><br />
+
+                <InputLabel 
+            id="demo-simple-select-autowidth-label">Initial Patient Aquity</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <InputLabel 
+                id="demo-simple-select-autowidth-label">Provider's Primary Impression</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
+                <p>Last Known Well calendar goes here</p>
 
         </div>
     )
