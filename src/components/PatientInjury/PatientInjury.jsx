8ba95c77acc57856f11c34343394b19a3db78319
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-23 10:21:51 -0500

    added dropdown for Cause of Injury form

--- src/components/PatientInjury/PatientInjury.jsx
+++ src/components/PatientInjury/PatientInjury.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
+// ----- Material UI -----
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+
+
 function PatientInjury() {
 
     return (
         <div className="container">
             <h2>Patient Injury Form:</h2>
-            <button>What's Gucci</button>
+            <button>What's Gucci</button><br /><br />
+
+            <InputLabel 
+                id="demo-simple-select-autowidth-label">Cause of Injury</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Option 1</MenuItem>
+                    <MenuItem value={20}>Option 2</MenuItem>
+                    <MenuItem value={30}>Option 3</MenuItem>
+                </Select> <br /><br />
+
         </div>
     )
 }
