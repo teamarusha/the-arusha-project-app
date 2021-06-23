0a62655898646611b357a065a732be8449386f13
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-23 09:10:23 -0500

    added input and select fields for Demographics view

--- src/components/PatientDemographics/PatientDemographics.jsx
+++ src/components/PatientDemographics/PatientDemographics.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 
+// ---- Material UI ----
+import { TextField } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+
 function PatientDemographics() {
 
     return (
         <div className="container">
             <h2>Patient Demographics Form:</h2>
-            <button>Yooooo</button>
+            <button>Yooooo</button> <br /><br />
+
+            <TextField id="outlined-basic" label="First Name" variant="outlined">
+            </TextField> &nbsp;
+            <TextField id="outlined-basic" label="Last Name" variant="outlined">
+            </TextField> <br /><br />
+            
+            <InputLabel id="demo-simple-select-autowidth-label">Gender</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Male</MenuItem>
+                    <MenuItem value={20}>Female</MenuItem>
+                    <MenuItem value={30}>Other</MenuItem>
+                </Select> <br /><br />
+                <InputLabel id="demo-simple-select-autowidth-label">Race</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    // value={age}
+                    // onChange={handleChange}
+                    autoWidth
+                >
+                    <MenuItem value="">
+                    <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>African</MenuItem>
+                    <MenuItem value={20}>African-American</MenuItem>
+                    <MenuItem value={30}>American Indian</MenuItem>
+                </Select> <br /><br />
+
+            <TextField id="outlined-basic" label="State of Residence" variant="outlined">
+            </TextField> &nbsp;
+            <TextField id="outlined-basic" label="County of Residence" variant="outlined">
+            </TextField>
+
         </div>
     )
 }
--- src/components/PatientMedical/PatientMedical.jsx
+++ src/components/PatientMedical/PatientMedical.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+// ----- Material UI -----
+import { TextField } from '@material-ui/core';
+
 function PatientMedical() {
 
     return (
         <div className="container">
             <h2>Patient Medical History Form:</h2>
-            <button>Tell Me About Your Past</button>
+            <button>Tell Me About Your Past</button> <br /><br />
+
+            <TextField id="outlined-basic" label="State of Residence" variant="outlined">
+            </TextField> &nbsp;
+            <TextField id="outlined-basic" label="County of Residence" variant="outlined">
+            </TextField>
+
         </div>
     )
 }
