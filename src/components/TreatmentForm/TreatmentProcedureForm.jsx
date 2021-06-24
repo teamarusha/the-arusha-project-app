95382170530bc473b8accb651ada4dbae1de3d29
Robyn Noble <robynhurd09@gmail.com>
2021-06-24 13:28:49 -0500

    treatmentProcedures updated

--- src/components/TreatmentForm/TreatmentProcedureForm.jsx
+++ src/components/TreatmentForm/TreatmentProcedureForm.jsx
@@ -1,86 +1,128 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { useCookies } from "react-cookie";
+import { useHistory, useParams } from "react-router";
 
 //Material UI imports
 import { TextField, Button } from "@material-ui/core";
 
 const TreatmentProcedureForm = () => {
+    let [localTreatment, setLocalTreatment] = useState(JSON.parse(localStorage.getItem('treatment')));
+    let [render, setRender] = useState(false);
     const { id } = useParams();
+    const history = useHistory();
 
-    function cookieForm(props) {
-        const [ cookie, setCookie ] = useCookies([ 'procedureArray' ]);
-        let [ localCookie, setLocalCookie ] = useState( cookie );
-        let [ render, setRender ] = useState('');
-    }
-    // To render on page load
+    // Runs once when the component mounts
     useEffect(() => {
-        console.log( 'Params id:', id );
-        console.log( 'Cookie Mirror', localCookie );
-        console.log( 'THE cookie', cookie );
-        if( cookie[`${id}procedureArray`] >= 0 ) {
-            setRender( true );
+        // console.log('Params id:', id);
+        console.log('Cookie Mirror:', localTreatment);
+        console.log('Treatment Storage:', JSON.parse(localStorage.getItem('treatment')));
+        // If the user comes to this page without a patient ID, get redirected to patient 1
+        if (!id) {
+            history.push('/treatment/procedure/1');
         }
-    }, []);
+
+        // If they get redirected from patient 1 AND they have already entered values, skip this step
+        // If they are redirected here from /patients, initialize the first patient
+        else if (id == 1 && JSON.parse(localStorage.getItem('treatment')) === null) {
+            setLocalTreatment({ "1treatmentArray": [1], "1procedure1": '', "1procedureAttempts1": '', '1birthDate': '', '1bloodType': '' });
+            setRender(true);
+        }
+
+        // Otherwise, we allow the render as there should be data in storage
+        else {
+            setRender(true);
+        }
+        }, []);
     
-    function submitCookie(newCookie) {
-        console.log('setting THE cookie', newCookie.key, newCookie.item );
-        setLocalCookie({ ...localCookie, [ newCookie.key ] : newCookie.item });
-        setCookie( newCookie.key, newCookie.item, { path: '/' });
+   // Runs whenever localPatientMirror is changed, updated, manipulated at all
+    // This way they will always be the same as long as it is the mirror that is being changed
+    useEffect(() => {
+        console.log('UPDATING browser storage', localTreatment);
+        localStorage.setItem('treatment', JSON.stringify(localTreatment));
+    }, [localTreatment]);
+
+      // Only handles when a value is changed by keystroke/inputfield clicks. 
+    // Does NOT handle initialization of new data.
+    function submitValue(newParameter) {
+        console.log('Updating parameter in submitValue', newParameter.key, newParameter.thing);
+
+        setLocalTreatment({ ...localTreatment, [newParameter.key]: newParameter.thing });
+
+        // localStorage.setItem(`${newParameter.key}`, JSON.stringify(newParameter.thing));
     }
 
+    // This function handles initialization of new patients
     function addProcedure() {
-        if ( cookie[`${id}procedureArray`] === undefined) {
-            submitCookie({ key: `${id}procedure1`, item: '' });
-            submitCookie({ key: `${id}procedureAttempts`, item: '' });
-            submitCookie({ key: `${id}successfulProcedure`, item: '' });
-            submitCookie({ key: `${id}responseToProcedure`, item: '' });
-            submitCookie({ key: `${id}procedurePerformedBy`, item: '' });
-
-            submitCookie({ key: `${id}procedureArray`, item: [1] });
-            setRender(true);
-            // history.push(`/treatment/${id}`);
-        } else {
-            let newProcedureID = cookie.procedure.length + 1;
-            submitCookie({ key: `${id}procedure${newProcedureID}`, item: '' });
-            submitCookie({ key: `${id}procedureAttempts${newProcedureID}`, item: '' });
-            submitCookie({ key: `${id}successfulProcedure${newProcedureID}`, item: '' });
-            submitCookie({ key: `${id}responseToProcedure${newProcedureID}`, item: '' });
-            submitCookie({ key: `${id}procedurePerformedBy${newProcedureID}`, item: '' });
-            submitCookie({ key: `procedureArray`, item: [...cookie.procedure, newProcedureID] });
-        }
+        // newProcedureID will be the next procedure added. Does not use params from the url 
+        // Instead it looks at the treatmentArray and adds the next procedure based on that
+        // THIS ID IS NOT THE PROCEDURE'S ID FROM THE DATABASE,
+        // THIS VALUE MEANS NOTHING AFTER SUBMISSION
+        let newProcedureID = localTreatment.treatmentArray.length + 1;
+        console.log('new procedure ID', newProcedureID);
+        // This is where all parameters associated with a new patient get initialized
+        // We do this in order to not have inputs stagnate as you chose a new patient to edit
+        setLocalTreatment({
+            ...localTreatment,
+            'treatmentArray': [...localTreatment.treatmentArray, newProcedureID],
+            [newProcedureID + 'procedure1']: '',
+            [newProcedureID + 'procedureAttempts']: '',
+            [newProcedureID + 'successfulProcedure']: '',
+            [newProcedureID + 'responseToProcedure']: '',
+            [newProcedureID + 'procedurePerformedBy']: ''
+        })
+    }
+    // Simply redirects to the patient page of a new patient 
+    // based on their position in the patientArray
+    function changePatient(patientNumber) {
+        history.push(`/treatment/${patientNumber}`);
     }
 
     return(
         <div>
+            {
+                render ?
+                    localPatientMirror.patientArray.map(value =>
+                        <button key={`${value}changePatient`} disabled={id == value} onClick={() => changePatient(value)}>
+                            Edit Patient {value}
+                        </button>
+                    )
+                    :
+                    ''
+            }
+            
             <Button size="small" color="primary" variant="contained" onClick={addProcedure}>
                 Add Procedure
             </Button>
+
             <TextField id="outlined-basic" label="Procedure Attempted" 
-                variant="outlined" value={ localCookie[`${id}procedure`]}
-                onChange={( event ) => submitCookie({ key: `${id}procedure`,
-                item: event.target.value })}>
+                variant="outlined" value={ localTreatment[`procedure`]}
+                onChange={( event ) => submitValue({ key: `procedure`,
+                thing: event.target.value })}>
             </TextField>
+
             <TextField id="outlined-basic" label="Number of Procedure Attempts" 
-                variant="outlined" value={ localCookie[`${id}procedureAttempts`]}
-                onChange={( event ) => submitCookie({ key: `${id}procedureAttempts`,
-                item: event.target.value })}>
+                variant="outlined" value={ localTreatment[`procedureAttempts`]}
+                onChange={( event ) => submitValue({ key: `procedureAttempts`,
+                thing: event.target.value })}>
             </TextField>
+
             <TextField id="outlined-basic" label="Procedure Successful" 
-                variant="outlined" value={ localCookie[`${id}successfulProcedure`]}
-                onChange={( event ) => submitCookie({ key: `${id}successfulProcedure`,
-                item: event.target.value })}>
+                variant="outlined" value={ localTreatment[`successfulProcedure`]}
+                onChange={( event ) => submitValue({ key: `successfulProcedure`,
+                thing: event.target.value })}>
             </TextField>
+
             <TextField id="outlined-basic" label="Response to Procedure" 
-                variant="outlined" value={ localCookie[`${id}responseToProcedure`]}
-                onChange={( event ) => submitCookie({ key: `${id}responseToProcedure`,
-                item: event.target.value })}>
+                variant="outlined" value={ localTreatment[`responseToProcedure`]}
+                onChange={( event ) => submitValue({ key: `responseToProcedure`,
+                thing: event.target.value })}>
             </TextField>
+
             <TextField id="outlined-basic" label="Role/Type of Person Performing
-                Procedure" variant="outlined" value={ localCookie[`${id}procedurePerformedBy`]}
-                onChange={( event ) => submitCookie({ key: `$id}procedurePerformedBy`, 
-                item: event.target.value })}>
+                Procedure" variant="outlined" value={ localTreatment[`procedurePerformedBy`]}
+                onChange={( event ) => submitValue({ key: `procedurePerformedBy`, 
+                thing: event.target.value })}>
             </TextField>
+            
         </div>
     );
 };
