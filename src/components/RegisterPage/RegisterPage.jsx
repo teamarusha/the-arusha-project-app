a65a80550e6e6b9841ee553e5e5259b40ee13521
Christian Back <cdirt27@gmail.com>
2021-06-24 01:02:56 -0500

    completed registration refactoring

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,6 @@
+import { Button, Grid, ThemeProvider, Link } from '@material-ui/core';
 import React from 'react';
-
+import createMuiTheme from '../GLOBALUI/Theme'
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
@@ -7,21 +8,26 @@ function RegisterPage() {
   const history = useHistory();
 
   return (
-    <div>
+    <ThemeProvider theme={createMuiTheme}>
+      
+      
+      
       <RegisterForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
+      
+        <Link
+          variant='body1'
+          component="button"
           onClick={() => {
             history.push('/login');
           }}
+          color='secondary'
         >
           Login
-        </button>
-      </center>
-    </div>
+        </Link>
+      
+   
+    </ThemeProvider>
   );
 }
 
