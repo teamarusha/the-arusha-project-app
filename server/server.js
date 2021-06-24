149808746efa3917ee1e1df4f2c9e2a548fa9401
Charlie Stokes <chstokes7@gmail.com>
2021-06-24 11:22:09 -0500

    created branch

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const dropdownRouter = require('./routes/dropdown.router');
+const incidentRouter = require('./routes/incident.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/dropdown', dropdownRouter);
+app.use('/api/incident', incidentRouter);
 
 // Serve static files
 app.use(express.static('build'));
