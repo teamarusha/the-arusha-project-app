b5fe05266a2d0fb8275f4c2c6850557f8528f0ca
Robyn Noble <robynhurd09@gmail.com>
2021-06-25 14:22:36 -0500

    MUI in progress, busted code

--- src/components/IncidentForm/IncidentFormResponse.jsx
+++ src/components/IncidentForm/IncidentFormResponse.jsx
@@ -4,104 +4,144 @@ import { useHistory, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 
 //Material UI imports
-import { Accordion, AccordionDetails, AccordionSummary, 
-     Grid, MenuItem, Paper, TextField, Typography } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { ExpandMoreIcon } from "@material-ui/icons/ExpandMore";
 
 import useStyles from "./Styles";
 
-const IncidentFormResponse = () => {
+const IncidentFormResponse = ({ localIncident, setLocalIncident }) => {
+  // let [localIncident, setLocalIncident] = useState(JSON.parse(localStorage.getItem('incident')));
+  let [render, setRender] = useState(false);
+  const { id } = useParams();
+  // const history = useHistory();
+  const dispatch = useDispatch();
+  const dropdowns = useSelector((store) => store.dropdowns);
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const [selectedId, setSelectedId] = useState(false);
 
-        let [localIncident, setLocalIncident] = useState(JSON.parse(localStorage.getItem('incident')));
-        // let [render, setRender] = useState(false);
-        const { id } = useParams();
-        // const history = useHistory();
-        const dispatch = useDispatch();
-        const dropdowns = useSelector(store => store.dropdowns);
-        const classes = useStyles();
-        const [ expanded, setExpanded ] = useState(false);
-        const [ selectedId, setSelectedId ] = useState(false);
+  // // To render on page load
+  // useEffect(() => {
+  //     // console.log( 'Params id:', id );
+  //     console.log( 'Cookie Mirror', localIncident );
+  //     console.log( 'Incident Storage', JSON.parse(localStorage.getItem('incident')));
 
-    // To render on page load
-    useEffect(() => {
-        // console.log( 'Params id:', id );
-        console.log( 'Cookie Mirror', localIncident );
-        console.log( 'Incident Storage', JSON.parse(localStorage.getItem('incident')));
-    }, []);
+  //     if ( JSON.parse(localStorage.getItem('incident')) === null) {
+  //         setLocalIncident({ "crew": '', "triageCat": '', 'serviceType': '' });
+  //         setRender(true);
+  //     }
+  // }, []);
 
-    useEffect(() => {
-        console.log('UPDATING browser storage', localIncident);
-        localStorage.setItem('incident', JSON.stringify(localIncident));
-    }, [localIncident]);
+  useEffect(() => {
+    console.log("UPDATING browser storage", localIncident);
+    localStorage.setItem("incident", JSON.stringify(localIncident));
+  }, [localIncident]);
 
-    // Only handles when a value is changed by keystroke/inputfield clicks. 
-    // Does NOT handle initialization of new data.
-    function submitValue(newParameter) {
-        console.log('Updating parameter in submitValue', newParameter.key, newParameter.thing);
-
-        setLocalIncident({ ...localIncident, [newParameter.key]: newParameter.thing });
-
-        // localStorage.setItem(`${newParameter.key}`, JSON.stringify(newParameter.thing));
-    }
-
-    let [localDropdownMirror, setLocalDropdownMirror] = useState(JSON.parse(localStorage.getItem('dropdowns')));
-
-    useEffect(() => {
-        if (JSON.parse(localStorage.getItem('dropdowns')) === null ){
-            dispatch({ type: 'GET_DROPDOWNS' })
-        } else {
-            dispatch({type: 'SET_DROPDOWNS', payload: localDropdownMirror})
-        }
-    }, []);
-
-    useEffect(() => {
-        if (dropdowns.go === true) {
-            localStorage.setItem('dropdowns', JSON.stringify(dropdowns));
-        }
-    }, [dropdowns.go]);
-
-    const handleListItemClick = (event, id) => {
-        setSelectedId(id);
-      };
-    
-      const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-      };
-
-    return (
-        <div>
-            {/* <TextField id="outlined-basic" label="Name of Crew" variant="outlined" 
-                value={ localIncident[`crew`]} onChange={( event ) => 
-                submitValue({ key: `crew`, 
-                thing: event.target.value })}>
-            </TextField> */}
-
-            { dropdowns.go &&
-            <div>
-            <TextField id="outlined-basic" select label="Triage Category" variant="outlined" 
-                value={ localIncident[`triageCat`]} onChange={( event ) => 
-                submitValue({ key: `triageCat`, thing: event.target.value })}>
-            {dropdowns['triage_cat'].map(item => 
-                <MenuItem key={'triage_cat'+ item.id} value={item.id}>
-                    {item.type}
-                </MenuItem>)
-            }
-            </TextField>
-
-            <TextField id="outlined-basic" select label="Type of Service Requested" 
-                variant="outlined" value={ localIncident[`serviceType`]}
-                onChange={( event ) => submitValue({ key: `serviceType`, 
-                thing: event.target.value })}>
-            {dropdowns['incident_type'].map(item => 
-                <MenuItem key={'incident_type'+ item.id} value={item.id}>
-                    {item.type}
-                </MenuItem>)
-            }
-            </TextField>
-            </div>
-            }
-        </div>
+  // Only handles when a value is changed by keystroke/inputfield clicks.
+  // Does NOT handle initialization of new data.
+  function submitValue(newParameter) {
+    console.log(
+      "Updating parameter in submitValue",
+      newParameter.key,
+      newParameter.thing
     );
+
+    setLocalIncident({
+      ...localIncident,
+      [newParameter.key]: newParameter.thing,
+    });
+
+    // localStorage.setItem(`${newParameter.key}`, JSON.stringify(newParameter.thing));
+  }
+
+  let [localDropdownMirror, setLocalDropdownMirror] = useState(
+    JSON.parse(localStorage.getItem("dropdowns"))
+  );
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("dropdowns")) === null) {
+      dispatch({ type: "GET_DROPDOWNS" });
+    } else {
+      dispatch({ type: "SET_DROPDOWNS", payload: localDropdownMirror });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (dropdowns.go === true) {
+      localStorage.setItem("dropdowns", JSON.stringify(dropdowns));
+    }
+  }, [dropdowns.go]);
+
+//   const handleListItemClick = (event, id) => {
+//     setSelectedId(id);
+//   };
+
+//   const handleChange = (panel) => (event, isExpanded) => {
+//     setExpanded(isExpanded ? panel : false);
+//   };
+
+  return (
+    <div>
+    { render && 
+      <TextField
+        id="outlined-basic"
+        label="Crew Id"
+        variant="outlined"
+        value={localIncident[`crew`]}
+        onChange={(event) =>
+          submitValue({ key: `crew`, thing: event.target.value })
+        }
+      ></TextField>
+        
+      {dropdowns.go && (
+        <div>
+          <TextField
+            id="outlined-basic"
+            select
+            label="Triage Category"
+            variant="outlined"
+            value={localIncident[`triageCat`]}
+            onChange={(event) =>
+              submitValue({ key: `triageCat`, thing: event.target.value })
+            }
+          >
+            {dropdowns["triage_cat"].map((item) => (
+              <MenuItem key={"triage_cat" + item.id} value={item.id}>
+                {item.type}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            id="outlined-basic"
+            select
+            label="Type of Service Requested"
+            variant="outlined"
+            value={localIncident[`serviceType`]}
+            onChange={(event) =>
+              submitValue({ key: `serviceType`, thing: event.target.value })
+            }
+          >
+            {dropdowns["incident_type"].map((item) => (
+              <MenuItem key={"incident_type" + item.id} value={item.id}>
+                {item.type}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+      )}
+    }
+    </div>
+  );
 };
 
 export default IncidentFormResponse;
--- src/components/IncidentForm/IncidentHome.jsx
+++ src/components/IncidentForm/IncidentHome.jsx
@@ -1,42 +1,123 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 import IncidentFormResponse from "./IncidentFormResponse";
 import IncidentFormDisposition from "./IncidentFormDisposition";
 import IncidentFormScene from "./IncidentFormScene";
 
-import { Accordion, AccordionSummary, Grid, Paper, Typography } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import { ExpandMoreIcon } from "@material-ui/icons/ExpandMore";
 
 import useStyles from "./Styles";
 
 function IncidentHome() {
+  const classes = useStyles();
+  const { id } = useParams();
+  const [localIncident, setLocalIncident] = useState();
+  const [render, setRender] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [selectedId, setSelectedId] = useState(false);
 
-    const classes = useStyles();
-    const [ expanded, setExpanded ] = useState(false);
-    const [ selectedId, setSelectedId ] = useState(false);
+  const handleListItemClick = (event, id) => {
+    setSelectedId(id);
+  };
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-      };
+  const handleChange = (id) => {
+    // setExpanded(isExpanded ? panel : false);
+    setExpanded(expanded === id ? -1 : id);
+  };
 
-    return(
-        <Grid Container justify='center' className={classes.root}>
-            <Grid item xs={12} s={6} m={4}>
-                <Accordion
-                    expanded={expanded === id}
-                    key={id}
-                    onChange={handleChange(id)}
-                >
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.header}>"Incident Form"</Typography>
-                    </AccordionSummary>
-                    <Paper className={classes.paper}>
-                    </Paper>
-                </Accordion>
-            </Grid>
-        </Grid>
+  // To render on page load
+  useEffect(() => {
+    // console.log( 'Params id:', id );
+    console.log("Cookie Mirror", localIncident);
+    console.log(
+      "Incident Storage",
+      JSON.parse(localStorage.getItem("incident"))
     );
-};
+
+    if (JSON.parse(localStorage.getItem("incident")) === null) {
+      setLocalIncident({
+        crew: "",
+        triageCat: "",
+        serviceType: "",
+        destinationState: "",
+        destinationCounty: "",
+        destinationZipCode: "",
+        transportDisposition: "",
+        transportMethod: "",
+        transportMode: "",
+        destinationType: "",
+        patientNumbers: "",
+        incidentState: "",
+        incidentCounty: "",
+        incidentZipCode: "",
+        possibleInjury: "",
+        alcoholDrugIndicators: "",
+      });
+      setRender(true);
+    }
+  }, []);
+
+  return (
+    <Grid Container justify="center" className={classes.root}>
+      <Grid item xs={12} s={6} m={4}>
+        <Accordion
+          expanded={expanded === id}
+          key={id}
+          onChange={handleChange(id)}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.header}>"Incident Form"</Typography>
+          </AccordionSummary>
+          <Paper className={classes.paper}>
+            <AccordionDetails className={classes.rootExpanded}>
+              <List component="nav" aria-label="main mailbox folders">
+                <ListItem
+                  button
+                  selected={selectedId === id}
+                  onClick={(event) => handleListItemClick(event, id)}
+                >
+                  <ListItemText
+                    primary={IncidentFormResponse(
+                      localIncident,
+                      setLocalIncident,
+                      render
+                    )}
+                  />
+                </ListItem>
+                <ListItem
+                  button
+                  selected={selectedId === id}
+                  onClick={(event) => handleListItemClick(event, id)}
+                >
+                  <ListItemText primary={IncidentFormScene} />
+                </ListItem>
+                <ListItem
+                  button
+                  selected={selectedId === id}
+                  onClick={(event) => handleListItemClick(event, id)}
+                >
+                  <ListItemText primary={IncidentFormDisposition} />
+                </ListItem>
+              </List>
+            </AccordionDetails>
+          </Paper>
+        </Accordion>
+      </Grid>
+    </Grid>
+  );
+}
 
 export default IncidentHome;
