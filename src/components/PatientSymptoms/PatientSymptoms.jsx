bb040e12df00acf53e1b973b63c63b92c2c0594e
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-25 13:56:05 -0500

    imported dropdown lists from the DB

--- src/components/PatientSymptoms/PatientSymptoms.jsx
+++ src/components/PatientSymptoms/PatientSymptoms.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 // ----- Material UI -----
 import { TextField } from '@material-ui/core';
@@ -8,11 +10,32 @@ import { MenuItem } from '@material-ui/core';
 
 function PatientSymptoms() {
 
+    const dispatch = useDispatch();
+    const dropdowns = useSelector((store) => store.dropdowns);
+
+    let [localDropdownMirror, setLocalDropdownMirror] = useState(JSON.parse(localStorage.getItem('dropdowns')));
+
+    useEffect(() => {
+        if (JSON.parse(localStorage.getItem('dropdowns')) === null ){
+            dispatch({ type: 'GET_DROPDOWNS' })
+        } else {
+            dispatch({type: 'SET_DROPDOWNS', payload: localDropdownMirror})
+        }
+    }, []);
+
+    useEffect(() => {
+        if (dropdowns.go === true) {
+            localStorage.setItem('dropdowns', JSON.stringify(dropdowns));
+        }
+    }, [dropdowns.go]);
+
     return (
         <div className="container">
             <h2>Patient Symptoms Form:</h2>
             <button>What's Buggin' You</button><br /><br />
 
+            {dropdowns.go &&
+            <div>
             <InputLabel 
             id="demo-simple-select-autowidth-label">Chief Complaint Anatomic Location</InputLabel>
                 <Select
@@ -25,9 +48,8 @@ function PatientSymptoms() {
                     <MenuItem value="">
                     <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Option 1</MenuItem>
-                    <MenuItem value={20}>Option 2</MenuItem>
-                    <MenuItem value={30}>Option 3</MenuItem>
+                    {dropdowns['anatomic_location'].map(item => <MenuItem key={'anatomic_location'+ item.id} 
+                    value={item.id}>{item.type}</MenuItem>)}
                 </Select> <br /><br />
 
                 <InputLabel 
@@ -42,9 +64,8 @@ function PatientSymptoms() {
                     <MenuItem value="">
                     <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Option 1</MenuItem>
-                    <MenuItem value={20}>Option 2</MenuItem>
-                    <MenuItem value={30}>Option 3</MenuItem>
+                    {dropdowns['organ_system'].map(item => <MenuItem key={'organ_system'+ item.id} 
+                    value={item.id}>{item.type}</MenuItem>)}
                 </Select> <br /><br />
 
                 <p>Symptom Onset calendar goes here</p> <br />
@@ -66,9 +87,8 @@ function PatientSymptoms() {
                     <MenuItem value="">
                     <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Option 1</MenuItem>
-                    <MenuItem value={20}>Option 2</MenuItem>
-                    <MenuItem value={30}>Option 3</MenuItem>
+                    {dropdowns['initial_acuity'].map(item => <MenuItem key={'initial_acuity'+ item.id} 
+                    value={item.id}>{item.type}</MenuItem>)}
                 </Select> <br /><br />
 
                 <InputLabel 
@@ -83,12 +103,14 @@ function PatientSymptoms() {
                     <MenuItem value="">
                     <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Option 1</MenuItem>
-                    <MenuItem value={20}>Option 2</MenuItem>
-                    <MenuItem value={30}>Option 3</MenuItem>
+                    {dropdowns['primary_impression'].map(item => <MenuItem key={'primary_impression'+ item.id} 
+                    value={item.id}>{item.type}</MenuItem>)}
                 </Select> <br /><br />
 
                 <p>Last Known Well calendar goes here</p>
+
+            </div>
+        }
 
         </div>
     )
