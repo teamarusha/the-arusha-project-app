c15d2ac214c7244b477a445d6dbbc575347f2f4f
Chloe Everson <chloeliz93@gmail.com>
2021-06-25 15:23:43 -0500

    admin mui

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -9,19 +9,19 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 
 
 
-// const useStyles = makeStyles({
-//     root: {
-//         width: '100%',
-//     },
-//     container: {
-//         maxHeight: 440,
-//     },
-// });
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    container: {
+        maxHeight: 440,
+    },
+});
 
 
 
@@ -29,6 +29,7 @@ import { useHistory } from 'react-router';
 function Admin() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const classes = useStyles();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_REPORTS' })
@@ -42,17 +43,17 @@ function Admin() {
 
 
     // const classes = useStyles();
-    // const [page, setPage] = React.useState(0);
-    // const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    // const handleChangePage = (event, newPage) => {
-    //     setPage(newPage);
-    // };
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
 
-    // const handleChangeRowsPerPage = (event) => {
-    //     setRowsPerPage(+event.target.value);
-    //     setPage(0);
-    // };
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     function handleClick(row) {
         // history.push('/reports/${}')
@@ -60,70 +61,71 @@ function Admin() {
 
     return (
         // JSON.stringify(reports)
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Patient</th>
-                    <th>Responder</th>
-                    <th>Report</th>
-                </tr>
-            </thead>
-            <tbody>
-                {reports.map((row) =>
-                    <tr key={row.id}>
+        // <table>
+        //     <thead>
+        //         <tr>
+        //             <th>Date</th>
+        //             <th>Time</th>
+        //             <th>Patient</th>
+        //             <th>Responder</th>
+        //             <th>Report</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         {reports.map((row) =>
+        //             <tr key={row.id}>
 
-                        <td>{moment(row.unit_notified).format('DD/MM/YYYY')}</td>
-                        <td>{moment(row.unit_notified).format('HH:MM:SS')}</td>
-                        <td>{row.patient_first_name} {row.patient_last_name}</td>
-                        <td>{row.user_first_name} {row.user_last_name}</td>
-                        <td><button onClick={handleClick(row)}>Report</button></td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
-        //     <Paper className={classes.root}>
-        //     <TableContainer className={classes.container}>
-        //        <Table stickyHeader aria-label="sticky table">
-        //             <TableHead>
-        //                <TableRow>
-        //                   
-        //                         <TableCell>Date</TableCell>
-        
-        //                             
-        //                              
-        //                   
-        //                 </TableRow>
-        //             </TableHead>
-        //             <TableBody>
-        //                 {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-        //                     return (
-        //                         <TableRow hover role="checkbox" tabIndex={-1} key={row.}>
-        //                             {columns.map((column) => {
-        //                                 const value = row[column.id];
-        //                                 return (
-        //                                     <TableCell key={column.id} align={column.align}>
-        //                                         {column.format && typeof value === 'number' ? column.format(value) : value}
-        //                                     </TableCell>
-        //                                 );
-        //                             })}
-        //                         </TableRow>
-        //                     );
-        //                 })}
-        //             </TableBody>
-        //         </Table>
-        //     </TableContainer>
-        //     <TablePagination
-        //         rowsPerPageOptions={[10, 25, 100]}
-        //         component="div"
-        //         count={rows.length}
-        //         rowsPerPage={rowsPerPage}
-        //         page={page}
-        //         onChangePage={handleChangePage}
-        //         onChangeRowsPerPage={handleChangeRowsPerPage}
-        //     />
-        // </Paper>
+        //                 <td>{moment(row.unit_notified).format('DD/MM/YYYY')}</td>
+        //                 <td>{moment(row.unit_notified).format('HH:MM:SS')}</td>
+        //                 <td>{row.patient_first_name} {row.patient_last_name}</td>
+        //                 <td>{row.user_first_name} {row.user_last_name}</td>
+        //                 <td><button onClick={handleClick(row)}>Report</button></td>
+        //             </tr>
+        //         )}
+        //     </tbody>
+        // </table>
+            <Paper className={classes.root}>
+            <TableContainer className={classes.container}>
+               <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                       <TableRow>
+                          
+                                <TableCell>Date</TableCell>
+                                <TableCell>Time</TableCell>
+                                <TableCell>Patient</TableCell>
+                                <TableCell>Responder</TableCell>
+                                <TableCell>Report</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {reports.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    
+                                       
+                                    <TableCell>{moment(row.unit_notified).format('DD/MM/YYYY')}</TableCell>
+                                    <TableCell>{moment(row.unit_notified).format('HH:MM:SS')}</TableCell>
+                                    <TableCell>{row.patient_first_name} {row.patient_last_name}</TableCell>
+                                    <TableCell>{row.user_first_name} {row.user_last_name}</TableCell>
+                                    <TableCell><button onClick={handleClick(row)}>Report</button></TableCell>
+                                       
+                                 
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={reports.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
 
     )
 }
