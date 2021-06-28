2e6e903e9466279edd4e51eda9659c756e416af7
Robyn Noble <robynhurd09@gmail.com>
2021-06-28 16:20:00 -0500

    treatment home updated with MUI

--- src/components/TreatmentForm/TreatmentHome.jsx
+++ src/components/TreatmentForm/TreatmentHome.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
 
 import TreatmentMedsForm from "./TreatmentMedsForm";
 import TreatmentProcedureForm from "./TreatmentProcedureForm";
 
 //____________________Material UI Imports____________________
-// import useStyles from "./Styles";
+import useStyles from "./Styles";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function TreatmentHome() {
-  // const classes = useStyles();
-  const { id } = useParams();
+  const classes = useStyles();
   const dispatch = useDispatch();
   const dropdowns = useSelector((store) => store.dropdowns);
   const [localTreatment, setLocalTreatment] = useState(
@@ -19,15 +25,9 @@ function TreatmentHome() {
   );
   const [render, setRender] = useState(false);
   const [expanded, setExpanded] = useState(false);
-  const [selectedId, setSelectedId] = useState(false);
 
-  const handleListItemClick = (event, id) => {
-    setSelectedId(id);
-  };
-
-  const handleChange = (id) => {
-    // setExpanded(isExpanded ? panel : false);
-    setExpanded(expanded === id ? -1 : id);
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
   };
 
   // ____________________DROPDOWNS____________________
@@ -81,18 +81,109 @@ function TreatmentHome() {
     }
   }, []);
 
+  // Main Button
+  const [buttonText, setButtonText] = useState("Dispatched");
+  function clickMe() {
+    console.log("Button clicked...");
+
+    switch (buttonText) {
+      case "Dispatched":
+        setButtonText("Unit En Route");
+        break;
+      case "Unit En Route":
+        setButtonText("Arrived at Scene");
+        break;
+      case "Arrived at Scene":
+        setButtonText("Arrived at Patient");
+        break;
+      case "Arrived at Patient":
+        setButtonText("En Route to Hospital");
+        break;
+      case "En Route to Hospital":
+        setButtonText("Arrived at Hospital");
+        break;
+      default:
+        setButtonText("Dispatched");
+        break;
+    }
+
+    const timestamp = Date.now(); // This would be the timestamp you want to format
+    console.log(
+      new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }).format(timestamp)
+    );
+  }
+
   return (
-    <div>
-      <TreatmentMedsForm
-        localTreatment={localTreatment}
-        setLocalTreatment={setLocalTreatment}
-        render={render}
-      />
-      <TreatmentProcedureForm
-        localTreatment={localTreatment}
-        setLocalTreatment={setLocalTreatment}
-        render={render}
-      />
+    <div className="container">
+      <h2>Treament Form Home</h2>
+
+      <div>
+        <Button onClick={clickMe} color="primary" variant="contained">
+          {buttonText}
+        </Button>
+      </div>
+      <div className={classes.root}>
+        <Accordion
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+            style={{ textAlign: "center" }}
+          >
+            <Typography
+              classes={{ root: classes.text }}
+              className={classes.heading}
+            >
+              Medication Administered
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TreatmentMedsForm
+              localTreatment={localTreatment}
+              setLocalTreatment={setLocalTreatment}
+              render={render}
+            />
+          </AccordionDetails>
+        </Accordion>
+
+        <br />
+
+        <Accordion
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+            style={{ textAlign: "center" }}
+          >
+            <Typography
+              classes={{ root: classes.text }}
+              className={classes.heading}
+            >
+              Procedure Administered
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TreatmentProcedureForm
+              localTreatment={localTreatment}
+              setLocalTreatment={setLocalTreatment}
+              render={render}
+            />
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
   );
 }
