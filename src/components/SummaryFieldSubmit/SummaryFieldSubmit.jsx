8b83131f7a64172117228855dc22065c302f6a08
Robyn Noble <robynhurd09@gmail.com>
2021-06-30 22:44:53 -0500

    summary page started

--- src/components/SummaryFieldSubmit/SummaryFieldSubmit.jsx
+++ src/components/SummaryFieldSubmit/SummaryFieldSubmit.jsx
@@ -1,30 +1,68 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import TimestampButton from "../TimestampButton/TimestampButton";
+
+//____________________Material UI Imports____________________
+import { Button, TextField } from "@material-ui/core";
 
 function SummaryFieldSubmit(params) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const [incidentMirror, setIncidentMirror] = useState(
+    JSON.parse(localStorage.getItem("incident"))
+  );
+  const [patientsMirror, setPatientsMirror] = useState(
+    JSON.parse(localStorage.getItem("patients"))
+  );
+  const [treatmentMirror, setTreatmentMirror] = useState(
+    JSON.parse(localStorage.getItem("treatment"))
+  );
+  const [vitalsMirror, setVitalsMirror] = useState(
+    JSON.parse(localStorage.getItem("vitals"))
+  );
 
-    const [incidentMirror, setIncidentMirror] = useState(
-        JSON.parse(localStorage.getItem("incident"))
-      );
-      const [patientsMirror, setPatientsMirror] = useState(
-        JSON.parse(localStorage.getItem("patients"))
-      );
-      const [treatmentMirror, setTreatmentMirror] = useState(
-        JSON.parse(localStorage.getItem("treatment"))
-      );
-      const [vitalsMirror, setVitalsMirror] = useState(
-        JSON.parse(localStorage.getItem("vitals"))
-      );
+  // ____________________WATCHER FUNCTION____________________
+  useEffect(() => {
+    console.log("UPDATING browser storage", incidentMirror);
+    localStorage.setItem("incident", JSON.stringify(incidentMirror));
+  }, [incidentMirror]);
 
+  const submitSummary = (event) => {
+    event.preventDefault();
+    console.log("Summary submitted");
+    dispatch({
+      type: "POST_INCIDENT",
+      payload: {
+        incidentMirror: incidentMirror,
+        patientsMirror: patientsMirror,
+        treatmentMirror: treatmentMirror,
+        vitalsMirror: vitalsMirror,
+      },
+    });
+  };
 
-
-    return (
-        <div>
-            {/* <button onClick={submitIncident}></button> */}
-        </div>
-    )
+  return (
+    <div>
+      <TimestampButton
+        incidentMirror={incidentMirror}
+        setIncidentMirror={setIncidentMirror}
+      />
+      <br />
+      <br />
+      <TextField
+        id="outlined-basic"
+        label="Incident Summary"
+        variant="outlined"
+        value={incidentSummary}
+      ></TextField>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={submitSummary}
+      ></Button>
+    </div>
+  );
 }
 
 export default SummaryFieldSubmit;
